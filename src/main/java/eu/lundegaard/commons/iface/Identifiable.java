5ebf028fab3ee209eadd36be74b170357c42d680
Ales Rybak <ales.rybak@lundegaard.eu>
2019-07-25 15:12:19 +0200

    Switch to LGPL license

--- src/main/java/eu/lundegaard/commons/iface/Identifiable.java
+++ src/main/java/eu/lundegaard/commons/iface/Identifiable.java
@@ -1,8 +1,16 @@
 /*
- * Copyright (C) Lundegaard a.s. 2019 - All Rights Reserved
+ * Copyright (C) 2019 Lundegaard a.s., All Rights Reserved
  *
- * Proprietary and confidential. Unauthorized copying of this file, via any
- * medium is strictly prohibited.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; version 3.0 of the License.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * https://www.gnu.org/licenses/lgpl-3.0.html
  */
 package eu.lundegaard.commons.iface;
 
