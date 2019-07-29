dc2c42337d4d522d1e758e4bc45243e4c7df6ed0
Ales Rybak <ales.rybak@lundegaard.eu>
2019-07-29 15:07:57 +0200

    Reformatted license

--- src/test/groovy/eu/lundegaard/commons/util/ValidateUtilTest.groovy
+++ src/test/groovy/eu/lundegaard/commons/util/ValidateUtilTest.groovy
@@ -1,15 +1,14 @@
 /**
  * Copyright (C) 2019 Lundegaard a.s., All Rights Reserved
  *
- * This library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; version 3.0 of the
- * License.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; version 3.0 of the License.
  *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  *
  * https://www.gnu.org/licenses/lgpl-3.0.html
  */
